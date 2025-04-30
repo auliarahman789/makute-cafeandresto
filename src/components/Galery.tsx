3ec7865e5e616adf8ac03573a6cdb3ded4c6fa3c
Rully <mauliarahman214@gmail.com>
2025-04-30 22:59:43 +0700

    up

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -1,7 +1,6 @@
 // Gallery.tsx
 "use client";
 import React, { useState, useEffect } from "react";
-import CardGallery from "./CardGallery";
 import { motion, AnimatePresence } from "framer-motion";
 
 function Gallery() {
